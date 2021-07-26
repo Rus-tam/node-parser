a0588fcc7223372fc21468b446d5bacae43ed418
Rustam <ahmetov.rustam2011@gmail.com>
2021-07-26 20:08:20 +0500

    'second_commit'

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
+const request = require('request');
+const getAdsUrl = require('./mainParser');
 
-
-const start = async () => {
-    try {
-        const ads = await parser('https://www.avito.ru/ufa/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1');
-        console.log(ads);
-    } catch (e) {
-        console.log(e)
-    }
-
-    process.exit(0);
-}
-
-start();
--- mainParser.js
+++ mainParser.js
@@ -3,54 +3,86 @@ const cheerio = require('cheerio');
 
 const url = 'https://www.avito.ru/ufa/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1';
 
-const getAdsIfo = (res) => {
+
+const getAdsUrl = (res) => {
+    const baseUrl = 'https://www.avito.ru';
     let urls = [];
-    let titles = [];
+    let apartmentAdsUrl = [];
+    let apartmentTitles = [];
     let prices = [];
-    let addresses = [];
+    let titles = [];
+    let indexes = [];
+    let streatName = [];
+    let districtName = [];
+    let dates = [];
+    let adInfo = [];
     let $ = cheerio.load(res.body);
-    //Заголовок и ссылка на конкретное объявление
+
+    //Ссылка на конкретное объявление
     $('.link-link-39EVK').each((i, elem) => {
-        const url = $(elem).attr('href');
-        const title = $(elem).text();
-
-        urls.push(url);
-        titles.push(title);
+        urls.push($(elem).attr('href'));
+        titles.push($(elem).text());
     })
-    //Стоимость квартиры
+
+    //Фильтрация списка ссылок
+    //Определяем индексы ненужных элементов списка ссылок
+    urls.forEach((elem, index) => {
+        if (elem != undefined) {
+            elem.split('/').includes('ufa' && 'kvartiry') && (!elem.slice('/').includes('prodam')) ? indexes.push(index) : null;
+        }
+    })
+    //Создаем новые списки ссылок и заголовков без косячных вариантов
+    for (let i = 0; i < urls.length; i++) {
+        indexes.includes(urls.indexOf(urls[i])) ? apartmentAdsUrl.push(baseUrl.concat(urls[i])) : null;
+        indexes.includes(urls.indexOf(urls[i])) ? apartmentTitles.push(titles[i]) : null;
+    }
+
+    //Цены квартир
     $('.price-text-1HrJ_').each((i, elem) => {
-        const price = $(elem).text();
-
-        prices.push({
-            price
-        })
+        prices.push($(elem).text());
     })
-    //Адрес
+
+    //Адреса квартир
     $('.geo-address-9QndR').each((i, elem) => {
-        const address = $(elem).text();
-
-        addresses.push({
-            address
-        })
+        streatName.push($(elem).text());
+    })
+    $('.geo-georeferences-3or5Q').each((i, elem) => {
+        districtName.push($(elem).text());
     })
 
-    urls = urls.slice(6, urls.length);
-    titles = titles.slice(6, titles.length);
-    console.log(urls.length)
-    console.log(titles.length);
-    console.log(prices.length);
-    console.log(addresses.length);
+    //Время прошедшее с выставления объявления
+    $('.date-text-2jSvU').each((i, elem) => {
+        dates.push($(elem).text());
+    })
+
+    for (let i = 0; i < apartmentAdsUrl.length; i++) {
+        adInfo.push({
+            apartmentTitles: apartmentTitles[i],
+            apartmentAdsUrl: apartmentAdsUrl[i],
+            prices: prices[i],
+            streatName: streatName[i],
+            districtName: districtName[i],
+            dates: dates[i]
+        });
+    };
+
+    return adInfo;
 }
 
 const getPage = async (url) => {
+
     await request({url, headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}}, (error, response) => {
         if (error) {
-            console.log(error);
+            throw error;
         } else {
-            getAdsIfo(response);
-
+            //console.log(response);
+            const result = getAdsUrl(response);
+            console.log(result);
         }
     })
 }
 
+
 getPage(url);
+
+
